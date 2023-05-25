34fa6736e96af4ce24a766de5ed1cafc832bee44
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-25 19:01:50 +0300

    add questions

--- CSE344-Project2/src/Level3.java
+++ CSE344-Project2/src/Level3.java
@@ -1,1 +1,28 @@
 
+private String[] questions = {
+    "What is 2 + 2?",
+    "How many fingers does a person have?",
+    "What is 5 - 3?",
+    "How many sides does a square have?",
+    "What is 10 divided by 2?",
+    "How many legs does a dog have?",
+    "What is 3 + 1?",
+    "How many eyes does a person have?",
+    "What is 4 - 1?",
+    "How many days are there in a week?"
+};
+
+private String[][] optionsList = {
+    {"3", "4", "5", "6"},
+    {"5", "10", "20", "100"},
+    {"1", "2", "3", "4"},
+    {"2", "3", "4", "5"},
+    {"2", "3", "5", "10"},
+    {"2", "3", "4", "6"},
+    {"2", "3", "4", "5"},
+    {"1", "2", "3", "4"},
+    {"2", "3", "4", "5"},
+    {"5", "6", "7", "8"}
+};
+
+private String[] correctAnswers = {"4", "10", "2", "4", "5", "4", "4", "2", "3", "7"};
