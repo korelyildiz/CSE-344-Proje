da1663db3848797a4b6db318a021887acff928b6
Korel Yildiz <72300120+korelyildiz@users.noreply.github.com>
2023-05-25 21:57:55 +0300

    Update Level3.java

--- CSE344-Project2/src/Level3.java
+++ CSE344-Project2/src/Level3.java
@@ -1,28 +1,36 @@
 
-private String[] questions = {
-    "What is 2 + 2?",
-    "How many fingers does a person have?",
-    "What is 5 - 3?",
-    "How many sides does a square have?",
-    "What is 10 divided by 2?",
-    "How many legs does a dog have?",
-    "What is 3 + 1?",
-    "How many eyes does a person have?",
-    "What is 4 - 1?",
-    "How many days are there in a week?"
-};
+public class Level3 extends Level1{
+	
+	private String[] Q = {
+		    "What is 2 + 2?",
+		    "How many fingers does a person have?",
+		    "What is 5 - 3?",
+		    "How many sides does a square have?",
+		    "What is 10 divided by 2?",
+		    "How many legs does a dog have?",
+		    "What is 3 + 1?",
+		    "How many eyes does a person have?",
+		    "What is 4 - 1?",
+		    "How many days are there in a week?"
+		};
 
-private String[][] optionsList = {
-    {"3", "4", "5", "6"},
-    {"5", "10", "20", "100"},
-    {"1", "2", "3", "4"},
-    {"2", "3", "4", "5"},
-    {"2", "3", "5", "10"},
-    {"2", "3", "4", "6"},
-    {"2", "3", "4", "5"},
-    {"1", "2", "3", "4"},
-    {"2", "3", "4", "5"},
-    {"5", "6", "7", "8"}
-};
+	private String[][] OL = {
+		    {"3", "4", "5", "6"},
+		    {"5", "10", "20", "100"},
+		    {"1", "2", "3", "4"},
+		    {"2", "3", "4", "5"},
+		    {"2", "3", "5", "10"},
+		    {"2", "3", "4", "6"},
+		    {"2", "3", "4", "5"},
+		    {"1", "2", "3", "4"},
+		    {"2", "3", "4", "5"},
+		    {"5", "6", "7", "8"}
+		};
 
-private String[] correctAnswers = {"4", "10", "2", "4", "5", "4", "4", "2", "3", "7"};
+	private String[] CA = {"4", "10", "2", "4", "5", "4", "4", "2", "3", "7"};
+	
+	public Level3() {
+		super();
+		super.set_questions(Q, OL, CA);
+    }
+}
