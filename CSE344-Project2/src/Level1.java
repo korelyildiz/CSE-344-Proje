f71e0ac4727d56b9a7eb1f13a75b27488a87dd1c
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-25 18:42:40 +0300

    Update Level1.java

--- CSE344-Project2/src/Level1.java
+++ CSE344-Project2/src/Level1.java
@@ -1,117 +1,115 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Level1 extends JPanel {
-    private JLabel soruLabel;
-    private JButton[] secenekler;
-    private JLabel sayacLabel;
-    private JLabel puanLabel;
-    private JButton sonrakiButton;
+    private JLabel questionLabel;
+    private JButton[] options;
+    private JLabel timerLabel;
+    private JLabel scoreLabel;
+    private JButton nextButton;
     private Timer timer;
-    private int sure = 30;
-    private int puan = 0;
-    private int soruIndex = 0;
+    private int timeRemaining = 30;
+    private int score = 0;
+    private int questionIndex = 0;
 
-  private String[] sorular = {"Which species does a whale belong to?",
-        "Which mammal can fly?",
-        "Which is the largest land animal?",
-        "Which animal has no continent?",
-        "Which animal can cut off its tail?",
-        "Which mammal is poisonous?",
-        "Which animal makes loud noises?",
-        "Which is the fastest running animal?",
-        "Which is the longest living animal?",
-        "Which is the largest mammal?"};
+    private String[] questions = {"Which species does a whale belong to?",
+            "Which mammal can fly?",
+            "Which is the largest land animal?",
+            "Which animal has no continent?",
+            "Which animal can cut off its tail?",
+            "Which mammal is poisonous?",
+            "Which animal makes loud noises?",
+            "Which is the fastest running animal?",
+            "Which is the longest living animal?",
+            "Which is the largest mammal?"};
 
-private String[][] seceneklerListesi = {{"Invertebrate", "Reptile", "Bird", "Mammal"},
-        {"Bat", "Dog", "Cat", "Bird"},
-        {"Elephant", "Lion", "Rhinoceros", "Giraffe"},
-        {"Dolphin", "Shark", "Kangaroo", "Crocodile"},
-        {"Turtle", "Koala", "Panda", "Monkey"},
-        {"Penguin", "Bat", "Shark", "Snake"},
-        {"Owl", "Eagle", "Pigeon", "Fox"},
-        {"Leopard", "Horse", "Cheetah", "Wolf"},
-        {"Jellyfish", "Sea Otter", "Galapagos Tortoise", "Parrot"},
-        {"Kilimanjaro Spider", "Shark", "Tiger", "Blue Whale"}};
+    private String[][] optionsList = {{"Invertebrate", "Reptile", "Bird", "Mammal"},
+            {"Bat", "Dog", "Cat", "Bird"},
+            {"Elephant", "Lion", "Rhinoceros", "Giraffe"},
+            {"Dolphin", "Shark", "Kangaroo", "Crocodile"},
+            {"Turtle", "Koala", "Panda", "Monkey"},
+            {"Penguin", "Bat", "Shark", "Snake"},
+            {"Owl", "Eagle", "Pigeon", "Fox"},
+            {"Leopard", "Horse", "Cheetah", "Wolf"},
+            {"Jellyfish", "Sea Otter", "Galapagos Tortoise", "Parrot"},
+            {"Kilimanjaro Spider", "Shark", "Tiger", "Blue Whale"}};
 
-private String[] dogruCevaplar = {"Mammal", "Bat", "Elephant", "Dolphin", "Turtle", "Bat", "Owl", "Cheetah",
-        "Galapagos Tortoise", "Blue Whale"};
-
+    private String[] correctAnswers = {"Mammal", "Bat", "Elephant", "Dolphin", "Turtle", "Bat", "Owl", "Cheetah",
+            "Galapagos Tortoise", "Blue Whale"};
 
     public Level1() {
 
-        // Soru Label'ı oluştur
-        soruLabel = new JLabel(sorular[soruIndex]);
-        soruLabel.setBounds(200, 50, 400, 50);
-        soruLabel.setHorizontalAlignment(SwingConstants.CENTER); // Soru Label'ını ortala
-        soruLabel.setFont(new Font("Arial", Font.BOLD, 20));
-        add(soruLabel);
+        // Create the Question Label
+        questionLabel = new JLabel(questions[questionIndex]);
+        questionLabel.setBounds(200, 50, 400, 50);
+        questionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        questionLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        add(questionLabel);
 
-        // Seçenekler için Button'ları oluştur
-        secenekler = new JButton[4];
-        for (int i = 0; i < secenekler.length; i++) {
-            secenekler[i] = new JButton(seceneklerListesi[soruIndex][i]);
-            secenekler[i].setBounds(200, 150 + i * 60, 400, 50);
-            secenekler[i].setBackground(new Color(120, 180, 220)); // Butonların rengini ayarla
-            secenekler[i].setForeground(Color.WHITE); // Buton metin rengini ayarla
-            secenekler[i].setFont(new Font("Arial", Font.PLAIN, 18));
-            add(secenekler[i]);
+        // Create Buttons for Options
+        options = new JButton[4];
+        for (int i = 0; i < options.length; i++) {
+            options[i] = new JButton(optionsList[questionIndex][i]);
+            options[i].setBounds(200, 150 + i * 60, 400, 50);
+            options[i].setBackground(new Color(120, 180, 220));
+            options[i].setForeground(Color.WHITE);
+            options[i].setFont(new Font("Arial", Font.PLAIN, 18));
+            add(options[i]);
 
-            secenekler[i].addActionListener(new ActionListener() {
+            options[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    JButton secenek = (JButton) e.getSource();
-                    cevapla(secenek); // Cevabı kontrol et
-                    timer.stop(); // Sayaç durduruldu
+                    JButton option = (JButton) e.getSource();
+                    answer(option);
+                    timer.stop();
                 }
             });
         }
 
-        // Puan Label'ı oluştur
-        puanLabel = new JLabel("Puan: 0");
-        puanLabel.setBounds(200, 20, 100, 30);
-        puanLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        add(puanLabel);
+        // Create the Score Label
+        scoreLabel = new JLabel("Score: 0");
+        scoreLabel.setBounds(200, 20, 100, 30);
+        scoreLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        add(scoreLabel);
 
-        // Sayac Label'ı oluştur
-        sayacLabel = new JLabel("30");
-        sayacLabel.setBounds(550, 20, 50, 50);
-        sayacLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        add(sayacLabel);
+        // Create the Timer Label
+        timerLabel = new JLabel("30");
+        timerLabel.setBounds(550, 20, 50, 50);
+        timerLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        add(timerLabel);
 
-        // Sonraki soru için Buton oluştur
-        sonrakiButton = new JButton("Sonraki Soru");
-        sonrakiButton.setBounds(330, 420, 140, 40);
-        sonrakiButton.setFont(new Font("Arial", Font.BOLD, 16));
-        sonrakiButton.setVisible(false);
-        add(sonrakiButton);
+        // Create the Next Question Button
+        nextButton = new JButton("Next Question");
+        nextButton.setBounds(330, 420, 140, 40);
+        nextButton.setFont(new Font("Arial", Font.BOLD, 16));
+        nextButton.setVisible(false);
+        add(nextButton);
 
-        sonrakiButton.addActionListener(new ActionListener() {
+        nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                soruIndex++;
-                if (soruIndex < sorular.length) {
-                    yeniSoru();
+                questionIndex++;
+                if (questionIndex < questions.length) {
+                    nextQuestion();
                 } else {
-                    sonucuGoster();
+                    showResult();
                 }
             }
         });
 
-        // Timer oluştur ve her 1 saniyede bir sayacı güncelle
+        // Create a Timer and update the timer label every 1 second
         timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                sure--;
-                sayacLabel.setText(String.valueOf(sure));
-                if (sure == 0) {
+                timeRemaining--;
+                timerLabel.setText(String.valueOf(timeRemaining));
+                if (timeRemaining == 0) {
                     timer.stop();
-                    sayacLabel.setText("Süre Bitti!");
-                    disableSecenekler(); // Zaman dolduğunda seçenek butonlarını devre dışı bırak
-                    sonrakiButton.setVisible(true); // Sonraki soru butonunu göster
+                    timerLabel.setText("Time's Up!");
+                    disableOptions();
+                    nextButton.setVisible(true);
                 }
             }
         });
@@ -120,50 +118,50 @@ private String[] dogruCevaplar = {"Mammal", "Bat", "Elephant", "Dolphin", "Turtl
         setVisible(true);
     }
 
-    private void cevapla(JButton secenek) {
-        disableSecenekler(); // Seçenek butonlarını devre dışı bırak
+    private void answer(JButton option) {
+        disableOptions();
 
-        for (int i = 0; i < secenekler.length; i++) {
-            if (secenekler[i] == secenek) { // Tıklanan butonu kontrol et
-                if (secenek.getText().equals(dogruCevaplar[soruIndex])) { // Doğru cevap
-                    secenek.setBackground(Color.GREEN); // Yeşil arkaplan
-                    puan += 10;
-                    puanLabel.setText("Puan: " + puan);
-                } else { // Yanlış cevap
-                    secenek.setBackground(Color.RED); // Kırmızı arkaplan
-                    for (int j = 0; j < secenekler.length; j++) {
-                        if (secenekler[j].getText().equals(dogruCevaplar[soruIndex])) {
-                            secenekler[j].setBackground(Color.GREEN); // Doğru cevabı yeşil yap
+        for (int i = 0; i < options.length; i++) {
+            if (options[i] == option) {
+                if (option.getText().equals(correctAnswers[questionIndex])) {
+                    option.setBackground(Color.GREEN);
+                    score += 10;
+                    scoreLabel.setText("Score: " + score);
+                } else {
+                    option.setBackground(Color.RED);
+                    for (int j = 0; j < options.length; j++) {
+                        if (options[j].getText().equals(correctAnswers[questionIndex])) {
+                            options[j].setBackground(Color.GREEN);
                             break;
                         }
                     }
                 }
             }
         }
-        sonrakiButton.setVisible(true); // Sonraki soru butonunu göster
+        nextButton.setVisible(true);
     }
 
-    private void disableSecenekler() {
-        for (JButton secenek : secenekler) {
-            secenek.setEnabled(false);
+    private void disableOptions() {
+        for (JButton option : options) {
+            option.setEnabled(false);
         }
     }
 
-    private void yeniSoru() {
-        soruLabel.setText(sorular[soruIndex]);
-        for (int i = 0; i < secenekler.length; i++) {
-            secenekler[i].setText(seceneklerListesi[soruIndex][i]);
-            secenekler[i].setEnabled(true);
-            secenekler[i].setBackground(new Color(120, 180, 220));
+    private void nextQuestion() {
+        questionLabel.setText(questions[questionIndex]);
+        for (int i = 0; i < options.length; i++) {
+            options[i].setText(optionsList[questionIndex][i]);
+            options[i].setEnabled(true);
+            options[i].setBackground(new Color(120, 180, 220));
         }
-        sure = 30;
-        sayacLabel.setText(String.valueOf(sure));
+        timeRemaining = 30;
+        timerLabel.setText(String.valueOf(timeRemaining));
         timer.restart();
-        sonrakiButton.setVisible(false);
+        nextButton.setVisible(false);
     }
 
-    private void sonucuGoster() {
-        JOptionPane.showMessageDialog(this, "Quiz Tamamlandı!\nToplam Puan: " + puan, "Sonuç", JOptionPane.INFORMATION_MESSAGE);
+    private void showResult() {
+        JOptionPane.showMessageDialog(this, "Quiz Completed!\nTotal Score: " + score, "Result", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
     }
 
