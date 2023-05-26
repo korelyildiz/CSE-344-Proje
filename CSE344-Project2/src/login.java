ce5fa503fe7bbb989783df726fa5029bb39aa49e
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-26 13:50:24 +0300

    gömülü sql login

--- CSE344-Project2/src/login.java
+++ CSE344-Project2/src/login.java
@@ -1,45 +1,62 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class login {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            // SQLite veritabanı sürücüsü yüklendi
 
-        String url = "jdbc:mysql://localhost:3306/project344";
-        String username = "root";
-        String password = "123456";
+            // Veritabanı bağlantısı yapma veya diğer işlemler devam eder...
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            // Sürücü yüklenirken bir hata oluştu
+        }
+        try {
+            // Veritabanı bağlantısı oluşturma
+            connection = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.print("Username: ");
-            String usernameInput = scanner.nextLine();
-            System.out.print("Password: ");
-            String passwordInput = scanner.nextLine();
+            // Kullanıcıdan giriş bilgilerini alma
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Kullanıcı adı: ");
+            String username = scanner.nextLine();
+            System.out.print("Şifre: ");
+            String password = scanner.nextLine();
 
-            if (!usernameInput.isEmpty() && !passwordInput.isEmpty()) {
-                String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
-                try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                    statement.setString(1, usernameInput);
-                    statement.setString(2, passwordInput);
-                    ResultSet resultSet = statement.executeQuery();
+            // Kullanıcıyı veritabanında arama
+            String selectQuery = "SELECT * FROM user WHERE username = ? AND password = ?";
+            preparedStatement = connection.prepareStatement(selectQuery);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            resultSet = preparedStatement.executeQuery();
 
-                    if (resultSet.next()) {
-                        System.out.println("Login successful.");
-                    } else {
-                        System.out.println("Error: Invalid username or password.");
-                    }
-                } catch (SQLException e) {
-                    System.out.println("Error on login: " + e.getMessage());
-                }
+            // Sonuçları kontrol etme
+            if (resultSet.next()) {
+                String userType = resultSet.getString("usertype");
+                System.out.println("Giriş başarılı! Kullanıcı tipi: " + userType);
             } else {
-                System.out.println("Error: Please enter both username and password.");
+                System.out.println("Giriş başarısız! Kullanıcı adı veya şifre hatalı.");
             }
         } catch (SQLException e) {
-            System.out.println("Error connecting to the database: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Kaynakları serbest bırakma
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
-
