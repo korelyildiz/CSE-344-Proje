5c6ed8bf453df2459b10ae516a4eb343ccb208f8
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-29 13:32:28 +0300

    Update Level2_3.java

--- CSE344-Project2/src/Level2_3.java
+++ CSE344-Project2/src/Level2_3.java
@@ -1,1 +1,34 @@
 
+private String[] Q = {
+    "Which river is the longest in the world?",
+    "Which country is both in Europe and Asia?",
+    "Which ocean is the largest?",
+    "Which country is known as the 'Land of the Rising Sun'?",
+    "Which mountain range is located in South America?",
+    "Which African country is known for its pyramids?",
+    "Which country is known for the Taj Mahal?",
+    "Which city is located on two continents?",
+    "Which sea is bordered by Greece, Turkey, and Italy?",
+    "Which country is known for the Great Barrier Reef?"
+};
+
+private String[] questions;
+
+private String[][] OL = {
+    {"Nile", "Amazon", "Mississippi", "Yangtze"},
+    {"Russia", "Turkey", "Japan", "Australia"},
+    {"Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"},
+    {"Japan", "China", "India", "South Korea"},
+    {"Andes", "Himalayas", "Rocky Mountains", "Alps"},
+    {"Egypt", "South Africa", "Nigeria", "Kenya"},
+    {"India", "China", "Egypt", "Brazil"},
+    {"Istanbul", "Cairo", "Moscow", "Dubai"},
+    {"Mediterranean Sea", "Black Sea", "Red Sea", "Caspian Sea"},
+    {"Australia", "Maldives", "Thailand", "Mexico"}
+};
+
+private String[][] optionsList;
+
+private String[] CA = {
+    "Nile", "Russia", "Pacific Ocean", "Japan", "Andes", "Egypt", "India", "Istanbul", "Mediterranean Sea", "Australia"
+};
