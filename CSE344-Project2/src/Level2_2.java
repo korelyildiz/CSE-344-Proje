92ac59e764defd6e7649794ac88e469be51d9c4b
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-29 13:32:19 +0300

    Update Level2_2.java

--- CSE344-Project2/src/Level2_2.java
+++ CSE344-Project2/src/Level2_2.java
@@ -1,1 +1,34 @@
 
+private String[] Q = {
+    "Which continent is known as the 'Land Down Under'?",
+    "Which continent is home to the Amazon Rainforest?",
+    "Which continent is known for the Sahara Desert?",
+    "Which continent is the largest in terms of land area?",
+    "Which continent is known for its pyramids and the Nile River?",
+    "Which continent is located south of Europe?",
+    "Which continent is known for its wildlife and the Serengeti?",
+    "Which continent is known for the Great Wall of China?",
+    "Which continent is home to the Statue of Liberty?",
+    "Which continent is known as the 'Frozen Continent'?"
+};
+
+private String[] questions;
+
+private String[][] OL = {
+    {"North America", "Asia", "Australia", "Africa"},
+    {"Africa", "South America", "Asia", "Europe"},
+    {"Africa", "South America", "Antarctica", "Australia"},
+    {"Asia", "North America", "Africa", "Europe"},
+    {"Africa", "Asia", "Australia", "South America"},
+    {"Asia", "Africa", "North America", "Europe"},
+    {"Africa", "Asia", "Australia", "South America"},
+    {"Asia", "Africa", "North America", "Europe"},
+    {"North America", "Asia", "Europe", "Australia"},
+    {"Antarctica", "Asia", "Africa", "Europe"}
+};
+
+private String[][] optionsList;
+
+private String[] CA = {
+    "Australia", "South America", "Africa", "Asia", "Africa", "Europe", "Africa", "Asia", "North America", "Antarctica"
+};
