1106ac139781b61eeb2d06712ff9a012bc4bcb06
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-29 13:31:26 +0300

    Update Level1_3.java

--- CSE344-Project2/src/Level1_3.java
+++ CSE344-Project2/src/Level1_3.java
@@ -1,1 +1,34 @@
 
+private String[] Q = {
+    "Which animal is known for its black and orange stripes?",
+    "Which bird can't fly and is known for its waddling walk?",
+    "Which animal has a long, flexible nose and lives in the jungle?",
+    "Which animal has a hard shell on its back and lives in the ocean?",
+    "Which animal is known for its long, curved horns?",
+    "Which bird is the symbol of peace?",
+    "Which animal has a long, sticky tongue and eats ants?",
+    "Which animal is known for its ability to camouflage?",
+    "Which animal is the national animal of Australia?",
+    "Which animal has a long, slender body and slithers on the ground?"
+};
+
+private String[] questions;
+
+private String[][] OL = {
+    {"Tiger", "Giraffe", "Zebra", "Tiger"},
+    {"Ostrich", "Penguin", "Eagle", "Hummingbird"},
+    {"Elephant", "Gorilla", "Kangaroo", "Monkey"},
+    {"Turtle", "Octopus", "Crab", "Dolphin"},
+    {"Elephant", "Giraffe", "Rhino", "Lion"},
+    {"Dove", "Eagle", "Owl", "Pigeon"},
+    {"Giraffe", "Elephant", "Tiger", "Anteater"},
+    {"Chameleon", "Turtle", "Kangaroo", "Lion"},
+    {"Kangaroo", "Koala", "Platypus", "Emu"},
+    {"Snake", "Elephant", "Giraffe", "Tiger"}
+};
+
+private String[][] optionsList;
+
+private String[] CA = {
+    "Tiger", "Penguin", "Elephant", "Turtle", "Giraffe", "Dove", "Anteater", "Chameleon", "Kangaroo", "Snake"
+};
