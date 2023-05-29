d9c6463456107ba9fe6cbaab3d6252b101225493
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-29 13:32:55 +0300

    Update Level3_3.java

--- CSE344-Project2/src/Level3_3.java
+++ CSE344-Project2/src/Level3_3.java
@@ -1,1 +1,35 @@
 
+private String[] Q = {
+    "What is the perimeter of a square with side length 8 units?",
+    "What is the area of a rectangle with length 12 units and width 5 units?",
+    "What is the circumference of a circle with a radius of 6 units? (Use π = 3.14)",
+    "What is the volume of a cube with side length 4 units?",
+    "What is the area of a triangle with base 10 units and height 8 units?",
+    "What is the perimeter of a rectangle with length 15 units and width 7 units?",
+    "What is the circumference of a circle with a diameter of 10 units? (Use π = 3.14)",
+    "What is the volume of a rectangular prism with length 6 units, width 3 units, and height 9 units?",
+    "What is the area of a circle with a radius of 5 units? (Use π = 3.14)",
+    "What is the perimeter of a triangle with sides of length 6 units, 8 units, and 10 units?"
+};
+
+private String[] questions;
+
+private String[][] OL = {
+    {"32 units", "40 units", "24 units", "64 units"},
+    {"60 square units", "50 square units", "72 square units", "30 square units"},
+    {"37.68 units", "18.84 units", "12.56 units", "31.42 units"},
+    {"64 cubic units", "16 cubic units", "48 cubic units", "32 cubic units"},
+    {"40 square units", "80 square units", "30 square units", "20 square units"},
+    {"44 units", "34 units", "32 units", "54 units"},
+    {"31.4 units", "15.7 units", "62.8 units", "21.98 units"},
+    {"162 cubic units", "48 cubic units", "72 cubic units", "54 cubic units"},
+    {"78.5 square units", "15.7 square units", "25 square units", "31.4 square units"},
+    {"24 units", "30 units", "28 units", "32 units"}
+};
+
+private String[][] optionsList;
+
+private String[] CA = {
+    "32 units", "60 square units", "37.68 units", "64 cubic units", "40 square units", 
+    "44 units", "31.4 units", "162 cubic units", "78.5 square units", "24 units"
+};
