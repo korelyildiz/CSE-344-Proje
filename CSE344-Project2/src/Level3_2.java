5a20bf6f8383fa46df8a140d2aa50c34a77418b2
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-29 13:32:40 +0300

    Update Level3_2.java

--- CSE344-Project2/src/Level3_2.java
+++ CSE344-Project2/src/Level3_2.java
@@ -1,1 +1,34 @@
 
+private String[] Q = {
+    "What is the sum of 5 and 3?",
+    "What is the result of 7 minus 2?",
+    "What is the product of 4 and 6?",
+    "What is the quotient of 12 divided by 3?",
+    "What is the sum of 9 and 2?",
+    "What is the result of 8 minus 5?",
+    "What is the product of 3 and 4?",
+    "What is the quotient of 15 divided by 5?",
+    "What is the sum of 6 and 7?",
+    "What is the result of 10 minus 3?"
+};
+
+private String[] questions;
+
+private String[][] OL = {
+    {"8", "7", "6", "9"},
+    {"5", "4", "3", "6"},
+    {"24", "30", "16", "10"},
+    {"4", "2", "3", "6"},
+    {"11", "10", "12", "9"},
+    {"3", "2", "4", "5"},
+    {"12", "6", "9", "7"},
+    {"3", "2", "5", "4"},
+    {"13", "14", "15", "12"},
+    {"7", "8", "9", "6"}
+};
+
+private String[][] optionsList;
+
+private String[] CA = {
+    "8", "5", "24", "4", "11", "3", "12", "3", "13", "7"
+};
