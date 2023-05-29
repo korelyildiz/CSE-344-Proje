a4f5eae2108752e600195d5fb5806d4ae7c394a6
MertYigit0 <107271196+MertYigit0@users.noreply.github.com>
2023-05-29 13:31:13 +0300

    Update Level1_2.java

--- CSE344-Project2/src/Level1_2.java
+++ CSE344-Project2/src/Level1_2.java
@@ -1,1 +1,34 @@
 
+private String[] Q = {
+    "Which animal has a hump on its back?",
+    "Which animal can swing from trees?",
+    "What is the largest species of bear?",
+    "Which animal has a long trunk and big ears?",
+    "Which animal is known for its black and white color and lives in China?",
+    "Which animal has a long neck and spots on its body?",
+    "Which animal is known for its web-spinning abilities?",
+    "Which animal can change the color of its skin?",
+    "Which animal has a shell and moves slowly?",
+    "Which animal is known for its long tongue and sticky saliva?"
+};
+
+private String[] questions;
+
+private String[][] OL = {
+    {"Camel", "Lion", "Gorilla", "Elephant"},
+    {"Lion", "Giraffe", "Monkey", "Spider"},
+    {"Polar bear", "Grizzly bear", "Koala bear", "Panda bear"},
+    {"Elephant", "Giraffe", "Horse", "Kangaroo"},
+    {"Panda", "Tiger", "Gorilla", "Zebra"},
+    {"Giraffe", "Leopard", "Turtle", "Zebra"},
+    {"Spider", "Butterfly", "Lion", "Snake"},
+    {"Chameleon", "Elephant", "Kangaroo", "Tiger"},
+    {"Turtle", "Lion", "Elephant", "Gorilla"},
+    {"Giraffe", "Tiger", "Elephant", "Chameleon"}
+};
+
+private String[][] optionsList;
+
+private String[] CA = {
+    "Camel", "Monkey", "Polar bear", "Elephant", "Panda", "Giraffe", "Spider", "Chameleon", "Turtle", "Chameleon"
+};
